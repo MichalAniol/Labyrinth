c79a9bf8903b5419599ae9e1ed177fc7ca68df49
Michał Anioł <32655609+MichalAniol@users.noreply.github.com>
2017-10-26 08:01:36 +0200

    Add files via upload

--- logic.cpp
+++ logic.cpp
@@ -72,6 +72,54 @@ int Labyrinth::looking_for_passage(int direction, int here_i_am_y, int here_i_am
 	return rezultat;
 }
 
+
+int Labyrinth::looking_for_the_way_out()
+{
+
+	int look_at_y = 0, look_at_x = 0, chamber_counter = 0;
+	int map[22][22];
+	int *adress_way = &way_out[0][0], *adres_map = &map[0][0], *adres_board = &board[0][0];
+	int sum_of_exits, chamber;
+
+	for (int i = 0; i < 300; i++) *adress_way++ = 0;						//cleanig of the way
+	for (int i = 0; i < 22 * 22; i++) *adres_map++ = *adres_board++;        //copy of the labyrinth
+
+	do
+	{
+
+		chamber = map[look_at_y][look_at_x];
+		sum_of_exits = (chamber & 1) + (chamber & 2) / 2 + (chamber & 4) / 4 + (chamber & 8) / 8;
+
+		if (sum_of_exits == 0)             // if it's death end go back to last cros
+		{
+			do {
+				chamber_counter--;
+				look_at_y = way_out[chamber_counter][0];
+				look_at_x = way_out[chamber_counter][1];
+			} while (map[look_at_y][look_at_x] == 0); continue;
+		}
+
+		way_out[chamber_counter][0] = look_at_y;               // note localisation at the way
+		way_out[chamber_counter][1] = look_at_x;
+
+
+		if (chamber & 2) { map[look_at_y][look_at_x] -= 2; look_at_x++; map[look_at_y][look_at_x] -= 8; chamber_counter++; continue; }
+
+		if (chamber & 4) { map[look_at_y][look_at_x] -= 4; look_at_y++; map[look_at_y][look_at_x] -= 1; chamber_counter++; continue; }
+
+		if (chamber & 1) { map[look_at_y][look_at_x] --  ; look_at_y--; map[look_at_y][look_at_x] -= 4; chamber_counter++; continue; }
+
+		if (chamber & 8) { map[look_at_y][look_at_x] -= 8; look_at_x--; map[look_at_y][look_at_x] -= 2; chamber_counter++; continue; }
+
+	} while (!(look_at_y == 21 && look_at_x == 21));
+
+	way_out[chamber_counter][0] = look_at_y;        // exit of labytrinth
+	way_out[chamber_counter][1] = look_at_x;
+
+	return ++chamber_counter;	// how logn the way is
+}
+
+
 void Labyrinth::moves()
 {
 	auto listener = EventListenerTouchOneByOne::create();
@@ -93,13 +141,18 @@ bool Labyrinth::Toucher(cocos2d::Touch *touch, cocos2d::Event *event)
 	auto push_the_buton2 = ScaleTo::create(.1f, .9f);
 	auto red_button1 = FadeTo::create(.6f, 255);
 	auto red_button2 = FadeTo::create(.2f, 0);
+	auto push_the_buton3 = ScaleTo::create(2.1f, 1 * .667f);
+	auto push_the_buton4 = ScaleTo::create(.1f, .9f * .667f);
+
 
 	auto move_ball_up1 = MoveBy::create(.1f, Point(0, 30));
 	auto move_ball_up2 = MoveBy::create(.1f, Point(0, 2));
 	auto move_all_walls_up = MoveBy::create(.3f, Point(0, -28));
+	auto move_the_way_out_up = MoveBy::create(.3f, Point(0, -28));
 	auto move_ball_down1 = MoveBy::create(.1f, Point(0, -30));
 	auto move_ball_down2 = MoveBy::create(.1f, Point(0, -2));
 	auto move_all_walls_down = MoveBy::create(.3f, Point(0, 28));
+	auto move_the_way_out_down = MoveBy::create(.3f, Point(0, 28));
 	auto move_ball_left = MoveBy::create(.1f, Point(-30, 0));
 	auto move_ball_right = MoveBy::create(.1f, Point(30, 0));
 
@@ -114,6 +167,7 @@ bool Labyrinth::Toucher(cocos2d::Touch *touch, cocos2d::Event *event)
 				{
 					ball->runAction(move_ball_up2);
 					all_walls->runAction(move_all_walls_up);
+					ariadnes_thread->runAction(move_the_way_out_up);
 				}
 				else ball->runAction(move_ball_up1);
 				button_up->runAction(push_the_buton1);
@@ -137,6 +191,7 @@ bool Labyrinth::Toucher(cocos2d::Touch *touch, cocos2d::Event *event)
 				{
 					ball->runAction(move_ball_down2);
 					all_walls->runAction(move_all_walls_down);
+					ariadnes_thread->runAction(move_the_way_out_down);
 				}
 				else ball->runAction(move_ball_down1);
 				button_down->runAction(push_the_buton1);
@@ -204,8 +259,27 @@ bool Labyrinth::Toucher(cocos2d::Touch *touch, cocos2d::Event *event)
 		Director::getInstance()->replaceScene(newScene);
 	}
 
-	return true;
+	// scene show me the way out
+	if (button_way_out->getBoundingBox().containsPoint(screen_touch))
+	{
+		button_way_out->runAction(push_the_buton3);
+		button_way_out->runAction(push_the_buton4);
 
+		if (ariadnes_thread->getOpacity())
+		{
+			auto visibility = FadeTo::create(2, 0);
+			ariadnes_thread->runAction(visibility);
+		}
+		else
+		{
+			auto visibility = FadeTo::create(2, 175);
+			ariadnes_thread->runAction(visibility);
+		}
+
+	}
+
+
+	return true;
 }
 
 int Labyrinth::surface_is_touched(int y, int x)
